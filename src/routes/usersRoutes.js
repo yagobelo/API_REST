e0d52eca30f126164e56bd5940831e2693d80136
yagobelo <yago.belo00@gmail.com>
2023-05-09 21:42:51 -0300

    CRUD complet

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -5,12 +5,16 @@ router.post("/", async (req, res) => {
   const { userName, email, password } = req.body;
 
   const verifyInvalidNewUser = [];
-  const existNameUser = await User.findOne({ userName: userName });
-  const existEmailUser = await User.findOne({ email: email });
 
   if (!userName) {
     verifyInvalidNewUser.push("Nome de usuario obrigatório!");
   }
+  if (userName.length < 4) {
+    verifyInvalidNewUser.push(
+      "Nome de usuario deve conter no minimo 4 caracteres!"
+    );
+  }
+  const existNameUser = await User.findOne({ userName: userName });
   if (existNameUser) {
     verifyInvalidNewUser.push("Nome de usuario ja existe!");
   }
@@ -18,6 +22,7 @@ router.post("/", async (req, res) => {
   if (!email) {
     verifyInvalidNewUser.push("Email obrigatório!");
   }
+  const existEmailUser = await User.findOne({ email: email });
   if (existEmailUser) {
     verifyInvalidNewUser.push("Email de usuario ja existe!");
   }
@@ -27,6 +32,23 @@ router.post("/", async (req, res) => {
   }
   if (password.length < 8) {
     verifyInvalidNewUser.push("Senha muito curta!");
+  }
+  const passwordContainUpperCase = /[A-Z]/.test(password);
+  if (!passwordContainUpperCase) {
+    verifyInvalidNewUser.push("Senha fraca, deve conter letra maiúscula!");
+  }
+  const passwordContainLowerCase = /[a-z]/.test(password);
+  if (!passwordContainLowerCase) {
+    verifyInvalidNewUser.push("Senha fraca, deve conter letra minúscula!");
+  }
+  const passwordContainNumber = /\d/.test(password);
+  if (!passwordContainNumber) {
+    verifyInvalidNewUser.push("Senha fraca, deve conter número!");
+  }
+  const passwordContainSpecialCharacter =
+    /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+  if (!passwordContainSpecialCharacter) {
+    verifyInvalidNewUser.push("Senha fraca, deve conter caractere especial!");
   }
 
   if (verifyInvalidNewUser.length > 0) {
@@ -47,11 +69,101 @@ router.post("/", async (req, res) => {
 });
 
 router.get("/", async (req, res) => {
+  const userName = req.query.userName;
   try {
-    const users = await User.find();
-    res.json(users);
+    if (!userName) {
+      const allUsers = await User.find();
+      if (!allUsers) {
+        return res.json("Nenhum usuario encontrado!");
+      }
+      res.json(allUsers);
+    } else {
+      const userByUsername = await User.findOne({ userName: userName });
+      if (!userByUsername) {
+        return res.json("Usuario não encontrado!");
+      }
+      res.json(userByUsername);
+    }
   } catch (error) {
     res.json("Erro ao buscar usuarios");
+    console.log(error);
+  }
+});
+
+router.patch("/", async (req, res) => {
+  const email = req.query.email;
+  const { userName, password } = req.body;
+  const verifyInvalidNewUser = [];
+
+  if (userName) {
+    const existNameUser = await User.findOne({ userName: userName });
+    if (existNameUser) {
+      verifyInvalidNewUser.push("Nome de usuario ja existe!");
+    }
+    if (userName.length < 4) {
+      verifyInvalidNewUser.push(
+        "Nome de usuario deve conter no minimo 4 caracteres!"
+      );
+    }
+  }
+
+  if (password) {
+    const lastPassword = await User.findOne({ password: password });
+    if (lastPassword) {
+      verifyInvalidNewUser.push(
+        "Senha não pode ser igual a última cadastrada!"
+      );
+    }
+    if (password.length < 8) {
+      verifyInvalidNewUser.push("Senha muito curta!");
+    }
+    const passwordContainUpperCase = /[A-Z]/.test(password);
+    if (!passwordContainUpperCase) {
+      verifyInvalidNewUser.push("Senha fraca, deve conter letra maiúscula!");
+    }
+    const passwordContainLowerCase = /[a-z]/.test(password);
+    if (!passwordContainLowerCase) {
+      verifyInvalidNewUser.push("Senha fraca, deve conter letra minúscula!");
+    }
+    const passwordContainNumber = /\d/.test(password);
+    if (!passwordContainNumber) {
+      verifyInvalidNewUser.push("Senha fraca, deve conter número!");
+    }
+    const passwordContainSpecialCharacter =
+      /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+    if (!passwordContainSpecialCharacter) {
+      verifyInvalidNewUser.push("Senha fraca, deve conter caractere especial!");
+    }
+
+    if (verifyInvalidNewUser.length > 0) {
+      return res.json(verifyInvalidNewUser);
+    }
+
+    try {
+      const user = {
+        userName,
+        password,
+      };
+      await User.updateOne({ email: email }, user);
+      res.json(user);
+    } catch (error) {
+      res.json("Erro interno ao atualizar usuario!");
+      console.log(error);
+    }
+  }
+});
+
+router.delete("/", async (req, res) => {
+  try {
+    const email = req.query.email;
+    const existUser = await User.findOne({ email: email });
+    if (!existUser) {
+      return res.json("Usuario não existe!");
+    }
+    await User.deleteOne({ email: email });
+    res.json("Usuario deletado!");
+  } catch (error) {
+    res.json("Erro interno ao deletar usuario!");
     console.log(error);
   }
 });
