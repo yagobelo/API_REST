fd39653694a0a4dd0ea0cd8c73d41cb90f29f081
yagobelo <yago.belo00@gmail.com>
2023-05-16 00:10:25 -0300

    adjust in connection with mongoose

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ const PORT = process.env.PORT;
 
 mongoose
   .connect(
-    `mongodb://${DB_USER}:${DB_PASSWORD}@ac-pn89l6z-shard-00-00.h9ghu3h.mongodb.net:27017,ac-pn89l6z-shard-00-01.h9ghu3h.mongodb.net:27017,ac-pn89l6z-shard-00-02.h9ghu3h.mongodb.net:27017/${DB_NAME}?ssl=true&replicaSet=atlas-2v89wv-shard-0&authSource=admin&retryWrites=true&w=majority`
+    `mongodb://${DB_USER}:${DB_PASSWORD}@ac-pn89l6z-shard-00-00.h9ghu3h.mongodb.net:27017,ac-pn89l6z-shard-00-01.h9ghu3h.mongodb.net:27017,ac-pn89l6z-shard-00-02.h9ghu3h.mongodb.net:27017/${DB_NAME}?ssl=true&replicaSet=atlas-2v89wv-shard-0&authSource=admin&retryWrites=true&w=majority` /* `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.h9ghu3h.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`*/,
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
     console.log("Connection with MongoDB: OK");
