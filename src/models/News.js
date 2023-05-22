f1ddea751fe284cd05ac23f29b8c0fef0c9fa500
yagobelo <yago.belo00@gmail.com>
2023-05-22 09:58:23 -0300

    add models News

---
+++ src/models/News.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const newsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+  text: {
+    type: String,
+    require: true,
+  },
+  banner: {
+    type: String,
+    require: true,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    require: true,
+  },
+  likes: {
+    type: Array,
+    require: true,
+  },
+  comments: {
+    type: Array,
+    require: true,
+  },
+});
+
+const News = mongoose.model("News", newsSchema);
+
+export default News;
