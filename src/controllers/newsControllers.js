6494d6fd106ff8ccedbe704717b587d65ffa028a
yagobelo <yago.belo00@gmail.com>
2023-05-26 12:28:17 -0300

    add pagination in find in router news

--- src/controllers/newsControllers.js
+++ src/controllers/newsControllers.js
@@ -24,11 +24,57 @@ const createNews = async (req, res) => {
 
 const findAllNews = async (req, res) => {
   try {
-    const news = await News.find();
+    let { limit, offset } = req.query;
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if (!limit) {
+      limit = 5;
+    }
+    if (!offset) {
+      offset = 0;
+    }
+
+    const news = await News.find()
+      .sort({ _id: -1 })
+      .skip(offset)
+      .limit(limit)
+      .populate("user");
+
+    const totalCountNews = await News.countDocuments();
+    const currentyUrl = req.baseUrl;
+
+    const next = offset + limit;
+    const nextUrl =
+      next < totalCountNews
+        ? `${currentyUrl}?limit=${limit}&offset=${next}`
+        : null;
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl =
+      previous != null
+        ? `${currentyUrl}?limit=${limit}&offset=${previous}`
+        : null;
+
     if (!news) {
       return res.status(400).json({ message: "Nenhuma noticia encontrada!" });
     }
-    res.status(200).json({ message: "Todas as notícias: ", news });
+    res.status(200).send({
+      nextUrl,
+      previousUrl,
+      limit,
+      offset,
+      totalCountNews,
+      results: news.map((item) => ({
+        id: item._id,
+        title: item.title,
+        description: item.description,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        userName: item.user.userName,
+      })),
+    });
   } catch (error) {
     res
       .status(401)
